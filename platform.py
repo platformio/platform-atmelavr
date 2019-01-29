c603eed92c6acc06a8ab980044de9e9a8ec89e07
Ivan Kravets <me@ikravets.com>
2019-01-29 22:09:33 +0200

    Revert "Use SourceForge CDN for CI"
    This reverts commit e78ecdaa564da0078f063d88fd913ba91a702bf7.

--- platform.py
+++ platform.py
@@ -12,22 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from os import getenv
-
 from platformio.managers.platform import PlatformBase
 
 
 class AtmelavrPlatform(PlatformBase):
-
-    @property
-    def package_repositories(self):
-        repositories = super(AtmelavrPlatform, self).package_repositories or []
-        if getenv("CI", "false").lower() == "true":
-            repositories = [
-                "https://sourceforge.net/projects/platformio-storage/"
-                "files/packages/manifest.json"
-            ] + repositories
-        return repositories
 
     def configure_default_packages(self, variables, targets):
         if not variables.get("board"):
